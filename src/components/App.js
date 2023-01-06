f38a03b4e107e89cd9c02a1c23ac757f95e81629
Jared Bain <jbbain22@gmail.com>
2023-01-06 15:02:11 -0500

    Update nav bar and add background video image

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './NavBar';
+import Main from './Main';
 
 function App() {
   return (
     <div className="App">
        <NavBar/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-      </header>
+       <Main/>
     </div>
   );
 }
